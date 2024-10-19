1677e42342f07af176aab7a20f1b467725e59fc3
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-10-19 13:03:07 +0500

    edite-1

--- main.js
+++
@@ -1,6 +0,0 @@
-var main  = document.querySelector("#main");
-var cur =document.querySelector(".cursor");
-main.addEventListener("mousemove" , function (acc) {
-     cur.style.left = acc.x +"px "
-     cur.style.top = acc.y +"px"
-})
