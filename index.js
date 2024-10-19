278741fbac42373920e97326a7f2c74e1a0ae586
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-10-19 13:13:39 +0500

    edite-2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var main  = document.querySelector("#main");
-var cur =document.querySelector(".cursor");
-main.addEventListener("mousemove" , function (acc) {
-     cur.style.left = acc.x +"px "
-     cur.style.top = acc.y +"px"
-})
+var main = document.querySelector("#main");
+var cur = document.querySelector(".cursor");
+
+main.addEventListener("mousemove", function (acc) {
+    cur.style.left = acc.x + "px";
+    cur.style.top = acc.y + "px";
+});
